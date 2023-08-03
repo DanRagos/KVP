19da394aaeab6604e57c3f024a9c24fbbf6b101e
DanRagos <dandanragos@gmail.com>
2023-08-03 17:19:29 +0800

    test

--- pages/profile.php
+++ pages/profile.php
@@ -584,6 +584,23 @@ $("body").on("click",".viewPms", function(e) {
   }
         });
 
+        $("body").on("click","#updateBtn", function(e) {
+	let sched_id = $(this).attr('data-id');
+	e.preventDefault();
+	var edit_id = $(this).attr('data-id');
+		$.ajax({
+			url: '../php/process.php',
+			method: 'post',
+			data: {sched_id : sched_id,
+			action:'updateSchedule'},
+			success: function (response) {
+			 $("#confirm-sched-modal .update_contents").html(response);
+			 $("#confirm-sched-modal").modal("show");
+
+			}
+		});
+	});
+
         
 function showSchedules(initialDate) {
     let user_idNotif = <?php echo $id; ?>;
